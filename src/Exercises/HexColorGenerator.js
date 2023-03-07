66931dbaa8386b4fe2157a973985eecf0aa1116d
iamsaief <saiefalemon@gmail.com>
2023-03-08 04:59:18 +0600

    exercise level 2-2

---
+++ src/Exercises/HexColorGenerator.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HexColorGenerator = (props) => {
+	return <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, cum.</div>;
+};
+
+export default HexColorGenerator;
